4cfaa07d93a5cd9f94b3c33ba1d36743db9060e1
Андрей Леготин <legotin4@yandex.ru>
2021-10-22 15:37:56 +0500

    add

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -26,7 +26,7 @@ class JustStaticPage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Очень простая страница'
-        context['text'] = f'На создание этой страницы уменя \
+        context['text'] = 'На создание этой страницы уменя \
                             ушло меньше 10 минут! Ай да я!'
         return context
 
