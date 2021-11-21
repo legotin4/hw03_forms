9906cfb233b626bc6ad8250ba6eb80843117aa32
Andrey Legotin <legotin4@yandex.ru>
2021-11-21 09:22:12 +0500

    add

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,15 +4,16 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     text = forms.CharField(
-        widget=forms.Textarea, 
-        label='Введите текст', 
-        required=True, 
+        widget=forms.Textarea,
+        label='Введите текст',
+        required=True,
         help_text='Текст поста'
-        )
+    )
+
     class Meta:
         model = Post
-        fields = ('text','group')
+        fields = ('text', 'group')
         labels = {'group': 'Выберите группу'}
-        help_texts={
-            'group':'Группа поста'
+        help_texts = {
+            'group': 'Группа поста'
         }
